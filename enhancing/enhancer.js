0ad48fc3337b49b2e9c599ec6a3c67a568f9387b
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-03-25 16:18:45 -0400

    repair and succeed functions complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,38 @@ module.exports = {
   get,
 };
 
+let sword = {
+  name: "Magic Sword",
+  durability: 80,
+  enhancement: 15
+}
+
+const club = {
+  name: "Big Billy",
+  durability: 50,
+  enhancement: 20
+}
+
 function succeed(item) {
+  if (item.enhancement === 20) {
+    return item;
+  } else {
+    item.enhancement += 1;
+  }
   return { ...item };
 }
 
 function fail(item) {
+
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
 function get(item) {
   return { ...item };
 }
+
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,33 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+let sword = {
+	name: "Magic Sword",
+	durability: 80,
+	enhancement: 15
+};
+
+const club = {
+	name: "Big Billy",
+	durability: 50,
+	enhancement: 20
+};
+
+// console.log("from tester:", enhancer.repair(club));
+
+describe(".repair()", function() {
+	it("should return the durability of item to 100", function() {
+		expect(enhancer.repair(club).durability).toBe(100);
+	});
+});
+
+// console.log("from tester:", enhancer.succeed(sword));
+
+describe(".succeed()", function() {
+	it("should increase enhancement by 1, maxed out at 20", function() {
+        expect(enhancer.succeed(sword).enhancement).toBe(16);
+        expect(enhancer.succeed(club).enhancement).toBe(20);
+        
+	});
+});
+
